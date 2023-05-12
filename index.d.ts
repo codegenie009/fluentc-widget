9f01888770e9752e56fb0e848fa8382dc362fa93
ghostson1120 <kachurihor111@gmail.com>
2023-05-12 19:27:18 +0300

    initial

--- index.d.ts
+++ index.d.ts
@@ -6,10 +6,18 @@ export interface FluentcWidgetInitOptions {
   widgettId: string;
 }
 
+export interface FluentcWidgetSetupOptions {
+  /**
+   * your FluentC widgetId
+   */
+  widgettId: string;
+}
+
 declare class fluentcWidget {
   constructor(options?: FluentcWidgetInitOptions);
   
   init(options?: FluentcWidgetInitOptions) : void;
+  setupWidget(options?: FluentcWidgetSetupOptions) : void;
 }
 
 export default fluentcWidget;
